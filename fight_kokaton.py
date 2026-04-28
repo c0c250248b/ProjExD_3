9b16c38f59174bee81ca76776c70dfe684ff03d9
C0C25024 <c0c250248b@edu.teu.ac.jp>
2026-04-28 14:20:49 +0900

    練習2

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -157,18 +157,26 @@ def main():
                 # スペースキー押下でBeamクラスのインスタンス生成
                 beam = Beam(bird)            
         screen.blit(bg_img, [0, 0])
+        if bomb is not None:
+            if bird.rct.colliderect(bomb.rct):
+                # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
+                bird.change_img(8, screen)
+                pg.display.update()
+                time.sleep(1)
+                return
         
-        if bird.rct.colliderect(bomb.rct):
-            # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
-            bird.change_img(8, screen)
-            pg.display.update()
-            time.sleep(1)
-            return
+        if bomb is not None:
+            if beam is not None:
+                if bird.rct.colliderect(bomb.rct):  # 練習2:衝突判定
+                    bomb = None
+                    beam = None
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
-        beam.update(screen)   
-        bomb.update(screen)
+        if beam is not None:
+            beam.update(screen)  
+        if bomb is not None: 
+            bomb.update(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
